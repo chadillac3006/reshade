f6054a7b7aaa20964a3853445554a6df3d996ea3
crosire <crosiredev@gmail.com>
2019-05-25 22:39:07 +0200

    Revert "Fix preset path selection in setup settings window to match new config option"
    This reverts commit 2dbc627c9bfa5f31c1ee446d63719cdb3f0a357e.

--- setup/Settings.xaml.cs
+++ setup/Settings.xaml.cs
@@ -22,6 +22,13 @@ namespace ReShade.Setup
 			InitializeComponent();
 			_configFilePath = configPath;
 
+			Presets.TextChanged += (s, e) =>
+			{
+				PresetOptions.Collection = (Presets.Text ?? "").Split(',')
+					.Where(f => !string.IsNullOrWhiteSpace(f))
+					.Select((f, i) => new PresetComboItem { Text = Path.GetFileName(f), Value = i });
+			};
+
 			BtnSave.Click += (s, e) => { Save(); Close(); };
 			BtnReload.Click += (s, e) => Load();
 			BtnCancel.Click += (s, e) => Close();
@@ -42,7 +49,8 @@ namespace ReShade.Setup
 
 		private void Save()
 		{
-			WriteValue("CurrentPresetPath", Preset.Text);
+			WriteValue("PresetFiles", Presets.Text);
+			WriteValue("CurrentPreset", PresetSelect.SelectedValue?.ToString());
 			WriteValue("EffectSearchPaths", EffectsPath.Text);
 			WriteValue("TextureSearchPaths", TexturesPath.Text);
 			WriteValue("ScreenshotPath", ScreenshotsPath.Text);
@@ -60,7 +68,8 @@ namespace ReShade.Setup
 		{
 			if (File.Exists(_configFilePath))
 			{
-				Preset.Text = ReadValue("CurrentPresetPath");
+				Presets.Text = ReadValue("PresetFiles");
+				PresetSelect.SelectedIndex = Convert.ToInt32(ReadValue("CurrentPreset", "-1")) + 1;
 				EffectsPath.Text = ReadValue("EffectSearchPaths");
 				TexturesPath.Text = ReadValue("TextureSearchPaths");
 				ScreenshotsPath.Text = ReadValue("ScreenshotPath");
@@ -85,16 +94,16 @@ namespace ReShade.Setup
 			}
 		}
 
-		private void BtnPreset_Clicked(object sender, RoutedEventArgs e)
+		private void BtnPresets_Clicked(object sender, RoutedEventArgs e)
 		{
-			string origFirstValue = (Preset.Text ?? "").Split(',')[0];
+			string origFirstValue = (Presets.Text ?? "").Split(',')[0];
 
 			var dlg = new OpenFileDialog
 			{
 				CheckFileExists = false,
 				CheckPathExists = true,
 				Multiselect = true,
-				Filter = "Preset Files (*.ini, *.txt)|*.ini;*.txt",
+				Filter = "Config Files (*.ini, *.txt)|*.ini;*.txt",
 				DefaultExt = ".ini",
 				InitialDirectory = string.IsNullOrWhiteSpace(origFirstValue) ? null : Path.GetDirectoryName(origFirstValue),
 				FileName = Path.GetFileName(origFirstValue)
@@ -102,7 +111,7 @@ namespace ReShade.Setup
 
 			if (dlg.ShowDialog(this) == true)
 			{
-				Preset.Text = string.Join(",", dlg.FileNames);
+				Presets.Text = string.Join(",", dlg.FileNames);
 			}
 		}
 
@@ -124,6 +133,12 @@ namespace ReShade.Setup
 			{
 				target.Tag = string.Join(",", dlg.FileNames);
 			}
+		}
+
+		// Force select the first item when there is no selection
+		private void PresetSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			PresetSelect.SelectedItem = PresetSelect.SelectedItem ?? PresetSelect.Items[0];
 		}
 
 		private static string CheckboxValue(bool? check) => (check.HasValue && check.Value ? "1" : "0");
